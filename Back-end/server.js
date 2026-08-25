4ecd82a99ad6e2a502de25416d534c552d8551b5
Lakshit-31 <lakshit031@gmail.com>
2026-08-25 14:56:58 +0530

    Initial EventHub project setup

---
+++ Back-end/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("EventHub Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
