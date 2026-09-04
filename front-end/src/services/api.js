023d85fddc01d492be64b8a5317d8ac2db191a51
Lakshit-31 <lakshit031@gmail.com>
2026-09-04 22:18:38 +0530

    issues fixed

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,21 +1,27 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://eventhub-backend-eofx.onrender.com/api',
 });
 
 // Interceptor to attach token to requests
-API.interceptors.request.use((config) => {
-  const user = localStorage.getItem('eventhub_user');
-  if (user) {
-    const parsed = JSON.parse(user);
-    if (parsed && parsed.token) {
-      config.headers.Authorization = `Bearer ${parsed.token}`;
+API.interceptors.request.use(
+  (config) => {
+    const user = localStorage.getItem('eventhub_user');
+
+    if (user) {
+      const parsed = JSON.parse(user);
+
+      if (parsed && parsed.token) {
+        config.headers.Authorization = `Bearer ${parsed.token}`;
+      }
     }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default API;
